0308a938a3a038a65c1a68478ee7bcfacca2eb11
nextdesusu <nextdesusu@gmail.com>
2020-04-23 00:52:36 +0400

    feat: now canvas is full screen even on resize of page

--- src/game/index.js
+++ src/game/index.js
@@ -2,10 +2,11 @@ const IB = require("./InterfaceBuilder");
 
 const startGame = (body, width, height) => {
   const Interface = new IB(body, width, height);
-  const Canvas = Interface.getCanvasInstance();
-  const ctx = Canvas.getContext("2d");
+  const ctx = Interface.ctx;
   ctx.fillStyle = "red";
   ctx.fillRect(0, 0, width, height);
+  ctx.fillStyle = "black";
+  ctx.fillRect(30, 100, 60, 200);
 };
 
 module.exports = startGame;
