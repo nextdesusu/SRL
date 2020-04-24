e597bb1361852b2ff811e7b76b9e277ac3bfb57d
nextdesusu <nextdesusu@gmail.com>
2020-04-25 00:35:25 +0400

    feat: done dialogue

--- src/game/index.js
+++ src/game/index.js
@@ -3,12 +3,26 @@ import GameCanvas from "./GameCanvas";
 
 const startGame = (body, width, height) => {
   const Canvas = new GameCanvas(body, width, height);
-  window.addEventListener("resize", () => Canvas.toScreenSize());
   const ctx = Canvas.ctx;
   ctx.fillStyle = "red";
   ctx.fillRect(0, 0, width, height);
   ctx.fillStyle = "black";
   ctx.fillRect(30, 100, 60, 200);
+
+  const Interface = new InterfaceBuilder(body, { system: "rgba(0, 0, 0, 0.3)" });
+  Interface.createDialogue("system");
+
+  window.addEventListener("resize", () => {
+    const { width, height } = window.screen;
+    Canvas.toScreenSize(width, height);
+    Interface.toScreenSize(width, height);
+  });
+
+  //Main cycle
+  while(true){
+    //Canvas.refresh();
+    break;
+  }
 };
 
 export default startGame;
